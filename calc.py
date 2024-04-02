914142f9c42bf1eef1583da4d7b4b1c70f239589
vitordias037 <vitao_ckp@hotmail.com>
2024-04-02 20:06:04 -0300

    calc

--- calc.py
+++ calc.py
@@ -7,5 +7,20 @@ print("3 - multiplicação:")
 print("4 - divisão:")
 
 operacao = input("digite a opção desejada: ")
-num1 = float(input(" digite o numero 1: "))
-num2 = float(input(" digite o numero 2: "))
+num1 = float (input(" digite o numero 1: "))
+num2 = float (input(" digite o numero 2: "))
+
+if operacao == "1" or  operacao == "adição":
+    print (num1 + num2)
+# adição
+elif operacao == "2" or operacao == "subtração":
+    print (num1 - num2)
+# subtrção
+elif operacao == "3" or operacao == "multiplicação":
+    print (num1 *  num2)
+# multiplicação
+elif operacao == "4" or operacao == "divisão":
+    print (num1  / num2)
+# divisão
+else:
+    print("você não digitou uma opção valida, tente novamente: ")
