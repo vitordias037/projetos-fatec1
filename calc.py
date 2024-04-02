86316bf0e5fe6c309913968a22f7d7ab2c47acbc
vitordias037 <vitao_ckp@hotmail.com>
2024-04-02 19:25:07 -0300

    calc

---
+++ calc.py
@@ -0,0 +1,11 @@
+num1: float 
+num2: float
+operacao: str
+print("1 - adição:")
+print("2 - subtração:")
+print("3 - multiplicação:")
+print("4 - divisão:")
+
+operacao = input("digite a opção desejada: ")
+num1 = float(input(" digite o numero 1: "))
+num2 = float(input(" digite o numero 2: "))
