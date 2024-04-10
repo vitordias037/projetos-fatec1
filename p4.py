945b5dce79fa635576a9417ce40f51dcb50b587f
vitordias037 <vitao_ckp@hotmail.com>
2024-04-09 22:20:22 -0300

    ordenador de numeros

--- p4.py
+++ p4.py
@@ -10,6 +10,11 @@ if num1 >= num2 and num2 >= num3:
     print (num3 , num2 , num1)
 elif num2 >= num1 and num1 >= num3:
     print (num3 , num1 , num2)
-
-else:
+elif num3 >= num2 and num2 >= num1:
     print (num1 , num2 , num3)
+elif num1 >= num3 and num3 >= num2:
+    print (num2 , num3 , num1)
+elif num2 >= num1 and num3 >= num1:
+    print (num1 , num3 , n2)
+else num3 >= num2 and num1 >= num2:
+    print (num2 , num1 , num3)
